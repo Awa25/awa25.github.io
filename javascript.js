a09868178dade56a7268595bec9286e9648ea09c
Awa25 <2020044@student.cct.ie>
2023-12-01 22:10:33 +0000

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,35 @@
+/*-------------------------
+Navigation Section
+-----------------------------*/
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.addEventListener('scroll', function () {
+
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
+
+
+
+
+/*----------------------------
+    SmoothScroll js
+-----------------------------*/
+  $(function() {
+    $('.custom-navbar a, #home a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+    });
+});
